2ea65a01d773e379754388264a91e3f129bdc0b0
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-03 16:01:16 +0900

    초기 커밋.

---
+++ day20_01/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day20_01
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Image myImage = Image.FromFile("photo.jpg");
+            e.Graphics.DrawImage(myImage, 0, 0);
+        }
+    
+    }
+}
